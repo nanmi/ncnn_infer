9f83379e489455c18fe5980c2739fe1aef86fdc2
nanmi <37356276+nanmi@users.noreply.github.com>
2021-07-12 17:04:19 +0800

    create repo and first commit

---
+++ tinydet.h
@@ -0,0 +1,162 @@
+//
+// Create by RangiLyu
+// 2020 / 10 / 2
+//
+
+#ifndef NANODET_H
+#define NANODET_H
+
+#include <opencv2/core/core.hpp>
+#include <net.h>
+
+typedef struct HeadInfo
+{
+    std::string cls_layer;
+    std::string dis_layer;
+    int stride;
+};
+
+typedef struct BoxInfo 
+{
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float score;
+    int label;
+} BoxInfo;
+
+const int color_list[80][3] =
+{
+    //{255 ,255 ,255}, //bg
+    {216 , 82 , 24},
+    {236 ,176 , 31},
+    {125 , 46 ,141},
+    {118 ,171 , 47},
+    { 76 ,189 ,237},
+    {238 , 19 , 46},
+    { 76 , 76 , 76},
+    {153 ,153 ,153},
+    {255 ,  0 ,  0},
+    {255 ,127 ,  0},
+    {190 ,190 ,  0},
+    {  0 ,255 ,  0},
+    {  0 ,  0 ,255},
+    {170 ,  0 ,255},
+    { 84 , 84 ,  0},
+    { 84 ,170 ,  0},
+    { 84 ,255 ,  0},
+    {170 , 84 ,  0},
+    {170 ,170 ,  0},
+    {170 ,255 ,  0},
+    {255 , 84 ,  0},
+    {255 ,170 ,  0},
+    {255 ,255 ,  0},
+    {  0 , 84 ,127},
+    {  0 ,170 ,127},
+    {  0 ,255 ,127},
+    { 84 ,  0 ,127},
+    { 84 , 84 ,127},
+    { 84 ,170 ,127},
+    { 84 ,255 ,127},
+    {170 ,  0 ,127},
+    {170 , 84 ,127},
+    {170 ,170 ,127},
+    {170 ,255 ,127},
+    {255 ,  0 ,127},
+    {255 , 84 ,127},
+    {255 ,170 ,127},
+    {255 ,255 ,127},
+    {  0 , 84 ,255},
+    {  0 ,170 ,255},
+    {  0 ,255 ,255},
+    { 84 ,  0 ,255},
+    { 84 , 84 ,255},
+    { 84 ,170 ,255},
+    { 84 ,255 ,255},
+    {170 ,  0 ,255},
+    {170 , 84 ,255},
+    {170 ,170 ,255},
+    {170 ,255 ,255},
+    {255 ,  0 ,255},
+    {255 , 84 ,255},
+    {255 ,170 ,255},
+    { 42 ,  0 ,  0},
+    { 84 ,  0 ,  0},
+    {127 ,  0 ,  0},
+    {170 ,  0 ,  0},
+    {212 ,  0 ,  0},
+    {255 ,  0 ,  0},
+    {  0 , 42 ,  0},
+    {  0 , 84 ,  0},
+    {  0 ,127 ,  0},
+    {  0 ,170 ,  0},
+    {  0 ,212 ,  0},
+    {  0 ,255 ,  0},
+    {  0 ,  0 , 42},
+    {  0 ,  0 , 84},
+    {  0 ,  0 ,127},
+    {  0 ,  0 ,170},
+    {  0 ,  0 ,212},
+    {  0 ,  0 ,255},
+    {  0 ,  0 ,  0},
+    { 36 , 36 , 36},
+    { 72 , 72 , 72},
+    {109 ,109 ,109},
+    {145 ,145 ,145},
+    {182 ,182 ,182},
+    {218 ,218 ,218},
+    {  0 ,113 ,188},
+    { 80 ,182 ,188},
+    {127 ,127 ,  0},
+};
+
+
+class TinyDet
+{
+public:
+    TinyDet(const char* param, const char* bin, bool useGPU);
+
+    ~TinyDet();
+
+    static TinyDet* detector;
+    ncnn::Net* Net;
+    static bool hasGPU;
+
+    // std::vector<HeadInfo> heads_info{
+    //     // cls_pred|dis_pred|stride
+    //     {"cls_pred_stride_8", "dis_pred_stride_8", 8},
+    //     {"cls_pred_stride_16", "dis_pred_stride_16", 16},
+    //     {"cls_pred_stride_32", "dis_pred_stride_32", 32},
+    // };
+    std::vector<HeadInfo> heads_info{
+        // cls_pred|dis_pred|stride
+        {"cls_8", "dis_8", 8},
+        {"cls_16", "dis_16", 16},
+        {"cls_32", "dis_32", 32},
+    };
+
+    std::vector<BoxInfo> detect(cv::Mat image, float score_threshold, float nms_threshold);
+
+    std::vector<std::string> labels{ "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+                                    "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+                                    "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+                                    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+                                    "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+                                    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+                                    "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                                    "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+                                    "hair drier", "toothbrush" };
+private:
+    void preprocess(cv::Mat& image, ncnn::Mat& in);
+    void decode_infer(ncnn::Mat& cls_pred, ncnn::Mat& dis_pred, int stride, float threshold, std::vector<std::vector<BoxInfo>>& results);
+    BoxInfo disPred2Bbox(const float*& dfl_det, int label, float score, int x, int y, int stride);
+    static void nms(std::vector<BoxInfo>& result, float nms_threshold);
+    int input_size[2] = {320, 320};
+    int num_class = 80;
+    int reg_max = 7;
+
+};
+
+
+#endif //NANODET_H
